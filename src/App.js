e212cb041bbcb0d223770765eebe32a5fc3d0a6c
oagbolade <you@example.com>
2019-11-19 18:50:00 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <button className="btn btn-success">Hello</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
